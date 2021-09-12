d0b1cbab0cc7b1e891edbe93c4d6432d3b8d6a71
Dev <82881439+Dev-Verissimo@users.noreply.github.com>
2021-09-12 10:22:22 -0300

    finalizando tarefa

--- index.js
+++ index.js
@@ -1,70 +1,71 @@
-// declarar componentes
-//    - ul que vai ser o pai para depois colocar as li
-//    - input do form com seu botão
-// CRIAR ELEMENTOS DE ACORDO COM O CLIQUE DO BUTÃO DO INPUT
-//    - li que vai ser posta dentro do ul 
-//    - criar input do tipo check, que vai transitar entre a fazer e feito, de acordo com o seu estado
+const inputFirst = document.getElementById('inputFirst')
+const botaoEnviar = document.getElementById('botaoEnviar')
 
-let inputFirst = document.getElementById('inputFirst')
-let botaoFirt = document.getElementById('botaoEnviar')
-let ulFazer = document.getElementById('aFazer')
-let ulFeito = document.getElementById('feito')
+const aFazer = document.getElementById('aFazer')
+const feito = document.getElementById('feito')
 
-let alteracoes = function criarBotoes() {
-  let alteracao = document.createElement('img')
-  alteracao.setAttribute('src','./pen.png')
-  alteracao.className = 'img'
-  let lixo = document.createElement('img')
-  lixo.setAttribute('src','./trash.png')
-  lixo.className = 'img'
-
-  let container = document.createElement('div')
-  container.className = 'div'
-  container.appendChild(alteracao)
-  container.appendChild(lixo)
-  return container
-}
-
-
-
-let criaLi = (e) => {
-  let li = document.createElement('li')
+const input = (li) => {
   let input = document.createElement('input')
-  let p = document.createElement('span')
   input.setAttribute('type', 'checkbox')
-  p.innerHTML = e.target.value
-
-  let container = document.createElement('div')
-  container.appendChild(input)
-  container.appendChild(p)
-  li.appendChild(container)
-  li.appendChild(alteracoes())
-  
-
-  isChecked(input, li)
-  return li
+  return input  
 }
 
-let isChecked = (input, li) => {
+let isChecked = (input, li, alteracao, p) => {
   input.addEventListener('change', () => {
     if(input.checked){
-     return ulFeito.appendChild(li)
-    }else 
-     return ulFazer.appendChild(li)
+      alteracao.remove()
+     return feito.appendChild(li)
+    }else {
+      return aFazer.appendChild(li)
+    }
+    
   })
 }
 
-inputFirst.addEventListener('change', (e) => {
-  botaoFirt.addEventListener('click', (event)=>{
-    event.preventDefault()
-    console.log(e.target.value)
-    let li = criaLi(e)
-    ulFazer.appendChild(li)
-    
-    if(e.target.value === '') {
-     return console.log(input.is(':checked'))
-    }
+botaoEnviar.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log(inputFirst.value)
+  let li = document.createElement('li')
+
+  let div1 = document.createElement('div')
+  let alteracao = document.createElement('img')
+  alteracao.setAttribute('src', './pen.png')
+
+  let lixo = document.createElement('img')
+  lixo.setAttribute('src','./trash.png')
+  
+    div1.appendChild(alteracao)
+    div1.appendChild(lixo)
+
+  let div2 = document.createElement('div')
+  let input = document.createElement('input')
+  input.setAttribute('type', 'checkbox')
+  let p = document.createElement('p')
+  
+  div2.appendChild(input)
+  p.textContent = inputFirst.value
+  div2.appendChild(p)
+  li.appendChild(div2)
+
+  li.appendChild(div1)
+
+
+  console.log(inputFirst.value)
+  aFazer.appendChild(li)
+
+  alteracao.addEventListener('click', () => {
+    li.remove()
+    inputFirst.value = p.textContent
+    inputFirst.style.borderBlockStartColor = 'red'
+    /*li.textContent = inputFirst.value*/
   })
+  lixo.addEventListener('click', () => {
+    li.remove()
 })
+  isChecked(input,li, alteracao)
+})
+
+
+
 
 
